d18f41511f639825ac2fb4351f67daeef04c8c82
Boom <boom2k31@gmail.com>
2024-03-28 16:43:27 +0700

    Finish Assign

--- ex-3.js
+++ ex-3.js
@@ -4,26 +4,25 @@
 const employee = {
   name: "John Doe",
   age: 18,
-  address: {
-    street: "123 Main Street",
-    city: "Cityville",
-    states: "Stateville",
-    country: "Countryland",
-    postalCode: "12345",
-  },
+  address_street: "123 Main Street",
+  address_city: "Cityville",
+  address_states: "Stateville",
+  address_country: "Countryland",
+  address_postalCode: "12345",
+  
 };
 
-console.log("Employee Name: " + employees.name);
+console.log("Employee Name: " + employee.name);
 console.log("Employee Age: " + employee.age);
 console.log(
   "Employee Address: " +
-    employee.address.street +
+    employee.address_street +
     ", " +
-    employee.address.city +
+    employee.address_city +
     ", " +
-    employee.address.state +
+    employee.address_states +
     ", " +
-    employee.address.country +
+    employee.address_country +
     " " +
-    employee.adress.postalCode
+    employee.address_postalCode
 );
